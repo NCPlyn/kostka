b6bd123e6874ea7c7278c7e9cd4d25753f4a8d33
NCPlyn <jakobabc@seznam.cz>
2020-10-19 12:52:07 +0200

    Další nastavení, statistiky, nový herní mód

--- js/main.js
+++ js/main.js
@@ -4,12 +4,24 @@ let pstats = document.getElementById('stats');
 let rf = document.getElementById('fast');
 let rs = document.getElementById('slow');
 let rm = document.getElementById('middle');
+let ru = document.getElementById('user');
+let ra = document.getElementById('auto');
 let audio = new Audio('roll.wav');
 let speed = 100;
+let hodNow = 1;
+let hody = [];
+let g;
+let h;
+let userhozeno = 0;
 
 kimg.addEventListener('click',function(){
-  getSpeed();
-  hod();
+  if(ru.checked == true && userhozeno == 1) {
+    h = 1;
+  } else if (userhozeno == 0) {
+    userhozeno = 1;
+    getSpeed();
+    hod();
+  }
 });
 
 function getSpeed() {
@@ -22,19 +34,66 @@ function getSpeed() {
   }
 }
 
+function getGameType() {
+  if(ra.checked == true) {
+    h = Math.floor(Math.random() * 30);
+  } else if (ru.checked == true) {
+    h = 999999;
+    ra.disabled = true;
+    ru.disabled = true;
+  }
+}
+
+function doAfter() {
+  ra.disabled = false;
+  ru.disabled = false;
+  userhozeno = 0;
+  pdone.innerHTML = "Done! You rolled " + g;
+  hodNow = g;
+  hody.push(g);
+  pstats.innerHTML = `<p>Poslední hod: ${hodNow}</p>`;
+  pstats.innerHTML += `<p>Počet hodů: ${hody.length}</p>`;
+  pstats.innerHTML += `<p>Součet všech hodů: ${celkem()}</p>`;
+  pstats.innerHTML += `<p>Průměr všech hodů: ${(celkem()/hody.length).toFixed(2)}</p>`;
+  pstats.innerHTML += `<p>Hozené maximum: ${max()}</p>`;
+  pstats.innerHTML += `<p>Hozené minimum: ${min()}</p>`;
+}
+
 function hod() {
   pdone.innerHTML = "Rolling...";
-  let h = Math.floor(Math.random() * 30);
-  var i = 0;
+  getGameType();
+  let i = 0;
   hhod();
   audio.play();
   function hhod(){
-    var g = Math.ceil(Math.random() * 6);
+    g = Math.ceil(Math.random() * 6);
     kimg.src = "img/" + g + ".png";
-    if(i==(h-1)) {
-      pdone.innerHTML = "Done! You rolled " + g;
-    }
+    if(i>=(h-1)) { doAfter(); } //funkce pro věci po hodu
     i++;
-    if (i < h) { setTimeout(hhod, speed); } //delay
+    if (i < h) { setTimeout(hhod, speed); } //delay a opakování
   }
+}
+
+function celkem() {
+  let out = 0;
+  for (let i=0;i<hody.length;i++) {
+    out += hody[i];
+  }
+  return out;
+}
+
+function max() {
+  let maximum = 1;
+  hody.forEach(function(value,index) {
+    if (value > maximum) maximum = value;
+  });
+  return maximum;
+}
+
+function min() {
+  let minimum = 1;
+  hody.forEach(function(value,index) {
+    if (value < minimum) minimum = value;
+  });
+  return minimum;
 }
