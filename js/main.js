bada59f25e10c41940376eb01b5fd47472fa97ab
NCPlyn <jakobabc@seznam.cz>
2020-10-19 12:08:12 +0200

    Vzhled trošku, rycholost, zvuk

--- js/main.js
+++ js/main.js
@@ -1,25 +1,40 @@
 let kimg = document.getElementById('kimg');
 let pdone = document.getElementById('done');
+let pstats = document.getElementById('stats');
+let rf = document.getElementById('fast');
+let rs = document.getElementById('slow');
+let rm = document.getElementById('middle');
+let audio = new Audio('roll.wav');
+let speed = 100;
 
 kimg.addEventListener('click',function(){
+  getSpeed();
   hod();
 });
 
+function getSpeed() {
+  if(rf.checked == true) {
+    speed = 10;
+  } else if (rm.checked == true) {
+    speed = 100;
+  } else if (rs.checked == true) {
+    speed = 300;
+  }
+}
+
 function hod() {
   pdone.innerHTML = "Rolling...";
-  const obrz = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png"];
   let h = Math.floor(Math.random() * 30);
-  var g;
   var i = 0;
   hhod();
+  audio.play();
   function hhod(){
-    g = Math.floor(Math.random() * obrz.length);
-    kimg.src = "img/" + obrz[g];
+    var g = Math.ceil(Math.random() * 6);
+    kimg.src = "img/" + g + ".png";
     if(i==(h-1)) {
-      pdone.innerHTML = "Done! You rolled " + (g+1);
+      pdone.innerHTML = "Done! You rolled " + g;
     }
     i++;
-    if (i < h) { setTimeout(hhod, 100); }
+    if (i < h) { setTimeout(hhod, speed); } //delay
   }
-  console.log("xd");
 }
